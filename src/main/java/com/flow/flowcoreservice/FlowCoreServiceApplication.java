183ee9bca2dde0f3406e2b5330c960a78f81eccd
Arundasak02 <arundasak02@gmail.com>
2025-12-05 14:58:42 +0530

    initialize flow-core-service project with Maven setup and basic application structure

---
+++ src/main/java/com/flow/flowcoreservice/FlowCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flow.flowcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowCoreServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowCoreServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/flow/flowcoreservice/FlowCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.flowcoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowCoreServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
